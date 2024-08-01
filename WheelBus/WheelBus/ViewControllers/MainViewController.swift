f4df14db5fd1e683d731370c5575006568a650d4
hjkim0905 <eax9952@gmail.com>
2024-08-01 16:43:48 +0900

    [Feat] : Project Set

---
+++ WheelBus/WheelBus/ViewControllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  WheelBus
+//
+//  Created by 김현중 on 8/1/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
